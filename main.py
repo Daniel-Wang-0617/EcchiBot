24c2639de1dd557971c2c68f95955eb88925cc0e
horsefeedapples <54728068+horsefeedapples@users.noreply.github.com>
2020-11-02 09:45:54 -0500

    Expanded sauce

--- main.py
+++ main.py
@@ -40,11 +40,11 @@ async def on_message(message):
     await bot.process_commands(message)
     if message.author == bot.user:
         return
-    if " im" in message.content:
-        responseIndex = message.content.find(" im ")
-        await message.channel.send("Hi " + message.content[responseIndex+2:] + ", I'm Jack")
+    if "shitij" in message.content.lower():
+        print(message.content)
 
 usedLinks = []
+usedSauce = []
 
 @bot.command(name="pic", help="gives a nice ecchi pic")
 async def pic(ctx):
@@ -78,13 +78,27 @@ async def pic(ctx):
 
 @bot.command(name="sauce", help="gives sauce :smirk:")
 async def sauce(ctx):
+    if(len(usedSauce)>5):
+        usedSauce.pop(0)
     sauce = [
-    ("https://imgur.com/a/x4qYloJ#keyug9l", 279963),
-    ("https://imgur.com/a/XIYB71N", 286444),
+    ("https://imgur.com/a/x4qYloJ#keyug9l", 279963, "After Relentlessly Cumming Inside a Runaway Gyaru, We Started Living Together as Fuck Buddies"),
+    ("https://imgur.com/a/XIYB71N", 286444, "Muramata-san's secret"),
+    ("https://imgur.com/a/Hl36PeL", 313039, "Welcome to the Residence with Glory Holes"),
+    ("https://imgur.com/a/eQR65xz", 324278, "I Am the Only Boy in Our Class"),
+    ("https://imgur.com/a/5mnTM03", 285588, "5th Year After School"),
+    ("https://imgur.com/a/3N8lh6J", "No sauce :pensive:", "Fucking the Gals Who Nonchalantly Came into My Room"),
+    ("https://imgur.com/a/UITDW", 285330, "My Classmate's Young Mom"),
+    ("https://imgur.com/a/VU2WX6v", 321889, "A Certain Day when a College Student was the Laziest"),
+    ("https://imgur.com/a/VJAXy7c", 298547, "Half Seduction"),
+    ("https://imgur.com/a/rE3hcVs", 318224, "It's So Hot That My Girlfriend Has To Blow Me Down"),
     ]
     response = random.choice(sauce)
+    while(response in usedSauce):
+        response = random.choice(sauce)
+    usedSauce.append(response)
     await ctx.send(response[0])
     await ctx.send("Sauce: " + str(response[1]))
+    await ctx.send("Title: " + response[2])
 
 @bot.command(name="hentai", help="gives sauce in a video format")
 async def sauce(ctx):
